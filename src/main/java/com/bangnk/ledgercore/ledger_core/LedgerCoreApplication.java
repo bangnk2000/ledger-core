b90b762ad5cf29cddf1a58bc3bf0ec5c5bf86602
bangnk <bangnk.2000@gmail.com>
2026-05-11 14:30:08 +0000

    chore:: initial project

---
+++ src/main/java/com/bangnk/ledgercore/ledger_core/LedgerCoreApplication.java
@@ -0,0 +1,13 @@
+package com.bangnk.ledgercore.ledger_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LedgerCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LedgerCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bangnk/ledgercore/ledger_core/LedgerCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bangnk.ledgercore.ledger_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedgerCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
